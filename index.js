5893ea6f7a38ec81bb531eaa0343b8d7cec753f6
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-12-24 20:47:35 -0300

    Corrigindo cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const Routes = require("./routes/routes");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const errorLogin = require("./middlewares/error");
@@ -8,9 +9,19 @@ const errorLogin = require("./middlewares/error");
 app.use(express.json());
 app.use(Routes);
 app.use(errorLogin);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  app.use(cors());
+  next();
+});
+
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
