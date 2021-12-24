fcb1e027d844e5e49027f44a712728ec716d3d70
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-12-24 20:02:11 -0300

    Erro cors

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const errorLogin = require("./middlewares/error");
 app.use(express.json());
 app.use(Routes);
 app.use(errorLogin);
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors());
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
