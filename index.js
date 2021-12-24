aaed7e2149b1a79262092a0a4c23df014f5403f5
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-12-24 19:56:18 -0300

    Corrigindo erro cors

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const Routes = require("./routes/routes");
+const cors = require("cors");
 require("dotenv").config();
 const errorLogin = require("./middlewares/error");
 
 app.use(express.json());
 app.use(Routes);
 app.use(errorLogin);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
